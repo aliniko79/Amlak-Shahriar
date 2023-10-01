7fd038f400bc6404b0c5fb74ea62e1fd94089a82
Ali nik <alinikoee1379@gmail.com>
2023-10-01 05:00:39 +0330

    Email Settings
    Email Settings

--- real_estate/real_estate/settings.py
+++ real_estate/real_estate/settings.py
@@ -133,3 +133,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR.joinpath('media')
+
+
+# Email settings
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'amlakshahriar1369@gmail.com'
+EMAIL_HOST_PASSWORD = 'amlakshahriai9631'
